012f73416b925edc1c2f77923beccccec6cc8d5e
Kavya N <kavyasubu1207@gmail.com>
2026-09-14 22:05:08 +0530

    Time: 387 ms (52.66%) | Memory: 155.3 MB (51.03%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,17 +1,40 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         int n=s.length();
-        Map<String,Integer> map=new HashMap<>();
+        Map<Integer,Integer> map=new HashMap<>();
         List<String> array=new ArrayList<>();
-        for(int i=0;i<=n-10;i++){
-            
-                String sub=s.substring(i,i+10);
-                map.put(sub,map.getOrDefault(sub,0)+1);
-            
+        int hash=0;
+        if(n<10)
+        return array;
+        for(int i=0;i<10;i++)
+        {
+            int x;
+            if(s.charAt(i)=='A')
+            x=0;
+            else if(s.charAt(i)=='C')
+            x=1;
+            else if(s.charAt(i)=='G')
+            x=2;
+            else
+            x=3;
+            hash=(hash<<2) | x;
         }
-        for(String sub :map.keySet()){
-            if(map.get(sub)>1)
-            array.add(sub);
+        map.put(hash,1);
+        for(int i=10;i<n;i++){
+            int x;
+            if(s.charAt(i)=='A')
+            x=0;
+            else if(s.charAt(i)=='C')
+            x=1;
+            else if(s.charAt(i)=='G')
+            x=2;
+            else
+            x=3;
+            hash=hash&((1<<18)-1);
+            hash=(hash<<2)|x;
+            map.put(hash,map.getOrDefault(hash,0)+1);
+            if(map.get(hash)==2)
+            array.add(s.substring(i-9,i+1));
         }
         return array;
     }
