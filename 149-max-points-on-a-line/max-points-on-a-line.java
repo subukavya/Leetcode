610c0f1f7377bbeecfd5da73715a83b1f94ca84f
Kavya N <kavyasubu1207@gmail.com>
2026-09-26 07:30:53 +0530

    Time: 43 ms (7.39%) | Memory: 43.1 MB (90.07%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+         int n = points.length;
+
+        if (n <= 2)
+            return n;
+
+        int max = 2;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int count = 2;
+
+                for (int k = 0; k < n; k++) {
+                    if (k == i || k == j)
+                        continue;
+
+                    if ((points[j][1] - points[i][1]) *
+                        (points[k][0] - points[i][0]) ==
+                        (points[k][1] - points[i][1]) *
+                        (points[j][0] - points[i][0])) {
+                        count++;
+                    }
+                }
+
+                max = Math.max(max, count);
+            }
+        }
+
+        return max;
+    }
+}
